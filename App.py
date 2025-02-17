d3234970b2719025a44c144206d538cdd7351e19
Aviwe Matoti <125872852+aviwematoti47@users.noreply.github.com>
2025-02-17 17:28:03 +0200

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st 
 
-st.header("This is mine")
+st.header("This is mine"):
 # Generate random time series data
 if st.button("Test this")
   time_series = np.random.randn(100)
