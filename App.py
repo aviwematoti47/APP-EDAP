676a5472b0547ef9e38b9de3d55235a0d4353724
Aviwe Matoti <125872852+aviwematoti47@users.noreply.github.com>
2025-02-17 17:26:54 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlit as st 
 
+st.header("This is mine")
 # Generate random time series data
-time_series = np.random.randn(100)
-
-# Plot the time series
-plt.plot(time_series)
-plt.title("Random 100-Unit Time Series")
-plt.xlabel("Time")
-plt.ylabel("Value")
-st.pyplot(plt.gcf())
+if st.button("Test this")
+  time_series = np.random.randn(100)
+  
+  # Plot the time series
+  plt.plot(time_series)
+  plt.title("Random 100-Unit Time Series")
+  plt.xlabel("Time")
+  plt.ylabel("Value")
+  st.pyplot(plt.gcf())
